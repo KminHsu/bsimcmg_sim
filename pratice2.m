3a0a0407a720ad46c30627bddc6c0f0b1ebbc1d6
kminhsu <kminhsu@localhost.localdomain>
2015-06-15 18:01:47 -0700

    Another RC network pratice

--- pratice2.m
+++ pratice2.m
@@ -8,15 +8,15 @@
 % C = [ 0 0 0; 0 1 0; 0 0 0 ]
 % W = [0 ;0 ;1]
 
-G = [ 1 -1 1 ; -1 1 0; 1 0 0 ]
-C = [ 0 0 0; 0 1 0; 0 0 0 ]
-W = [0 ;0 ;1]
+G = [ 1e3 -1e3 5 ; -1e3 1e3 0; 5 0 0 ]
+C = [ 0 0 0; 0 0.001e-6 0; 0 0 0 ]
+W = [0 ;0 ;5]
 % h = 0.001
-h = 5e-6
-len = 20000
-x = linspace(0.0001, 0.1, len);
-BE_y = linspace(0.0001, 0.1, len);
-TR_y = linspace(0.0001, 0.1, len);
+h = 1e-7
+len = 20
+x = linspace(0.0001, 2e-6, len);
+BE_y = linspace(0.0001, 2e-6, len);
+TR_y = linspace(0.0001, 2e-6, len);
 
 %%%BE
 X0 = inv(G)*W
@@ -31,27 +31,27 @@ for i = 1:length(x)
 end
 BE_X = X;
 
-%%%TR
-W = [0 ;0 ;1];
-X0 = inv(G)*W
-X = X0;
-M1 = ( G + 2/h*C );
-M2 = -1*( G - 2/h*C );
-for i = 1:length(x)
-  if i ==  floor(len/2)
-    W(3) = 0
-  end
-  X = inv(M1)*(M2*X + W + W);
-  TR_y(i) = X(2);
-end
-TR_X = X;
+% %%%TR
+% W = [0 ;0 ;10];
+% X0 = inv(G)*W
+% X = X0;
+% M1 = ( G + 2/h*C );
+% M2 = -1*( G - 2/h*C );
+% for i = 1:length(x)
+%   if i ==  floor(len/2)
+%     W(3) = 0
+%   end
+%   X = inv(M1)*(M2*X + W + W);
+%   TR_y(i) = X(2);
+% end
+% TR_X = X;
 
-figure
-plot(x, BE_y-TR_y)
+%figure
+%plot(x, BE_y-TR_y)
 figure
 plot(x, BE_y)
-figure
-plot(x, TR_y, '*')
+%figure
+%plot(x, TR_y, '*')
 
 %plot(x, BE_y, '*', x, TR_y, 'O')
 
