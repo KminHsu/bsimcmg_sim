bb9d86524a69924290204aeaac64c84dbdb6ec55
kminhsu <kminhsu@localhost.localdomain>
2015-06-17 06:46:01 -0700

    Fix bugs

--- pratice2.m
+++ pratice2.m
@@ -8,43 +8,35 @@
 % C = [ 0 0 0; 0 1 0; 0 0 0 ]
 % W = [0 ;0 ;1]
 
-G = [ 1e3 -1e3 5 ; -1e3 1e3 0; 5 0 0 ]
+G = [ 1e-3 -1e-3 1 ; -1e-3 1e-3 0; 1 0 0 ] + [ 0 0 0; 0 1e-3 0; 0 0 0]
 C = [ 0 0 0; 0 0.001e-6 0; 0 0 0 ]
-W = [0 ;0 ;5]
-% h = 0.001
-h = 1e-7
-len = 20
-x = linspace(0.0001, 2e-6, len);
-BE_y = linspace(0.0001, 2e-6, len);
-TR_y = linspace(0.0001, 2e-6, len);
+h = 1e-8
+x = 0:h:2e-6
+BE_y = zeros(length(x),1);
+TR_y = zeros(length(x),1);
 
 %%%BE
+W = [0; 0; vin(1)];
 X0 = inv(G)*W
 X = X0;
 M = ( G + 1/h * C );
-for i = 1:length(x)
-  if i ==  floor(len/2)
-    W(3) = 0
-  end
+for i = 2:length(x)
+  W = [0 ; 0; vin(x(i))];
   X = inv(M)*(1/h*C*X+W);
   BE_y(i) = X(2);
 end
-BE_X = X;
 
-% %%%TR
-% W = [0 ;0 ;10];
-% X0 = inv(G)*W
-% X = X0;
-% M1 = ( G + 2/h*C );
-% M2 = -1*( G - 2/h*C );
-% for i = 1:length(x)
-%   if i ==  floor(len/2)
-%     W(3) = 0
-%   end
-%   X = inv(M1)*(M2*X + W + W);
-%   TR_y(i) = X(2);
-% end
-% TR_X = X;
+%%%TR
+W = [0; 0; vin(1)];
+X0 = inv(G)*W;
+X = X0;
+M1 = ( G + 2/h*C );
+M2 = -1*( G - 2/h*C );
+for i = 2:length(x)
+  W = [0 ; 0; vin(x(i))];
+  X = inv(M1)*(M2*X + W + W);
+  TR_y(i) = X(2);
+end
 
 %figure
 %plot(x, BE_y-TR_y)
@@ -52,7 +44,6 @@ figure
 plot(x, BE_y)
 %figure
 %plot(x, TR_y, '*')
-
 %plot(x, BE_y, '*', x, TR_y, 'O')
 
 % %%%BE
--- vin.m
+++ vin.m
@@ -1,7 +1,7 @@
 function y = vin(t)
   if t == 0.0
     y = 0.0;
-  elseif t >= 1e-7 && t <= 5e-7
+  elseif t > 0 && t <= 5e-7
     y = 5.0;
   else
     y = 0.0;
