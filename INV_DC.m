cd51527152cad8b93960519aae3717f357849293
JimmYang <stu3181@hotmail.com>
2015-07-25 16:22:07 +0800

    Update INV_DC.m

--- INV_DC.m
+++ INV_DC.m
@@ -1,9 +1,9 @@
 function X = INV_DC(Vin, Vdd, error, alpha)
-
+  % chart 3-2: initialize the BSIMCMG, and read MOS model card.
   Initialize();
   CreateInst('MN', 'nmos1', 'L=3e-8 TFIN=1.5e-8 NFIN=10.0 NRS=1.0 NRD=1.0');
   CreateInst('MP', 'pmos1', 'L=3e-8 TFIN=1.5e-8 NFIN=10.0 NRS=1.0 NRD=1.0');
-  
+  % chart 3-3: initialize matrices and variables.
   G = [ zeros(3,3) eye(3,2);
         eye(2,3) zeros(2,2)
   ];
@@ -34,9 +34,10 @@ function X = INV_DC(Vin, Vdd, error, alpha)
   Vout = 10e-8;
   for loop = 1:20000
     W = [0; 0; 0; Vdd; Vin];
+    % chart 3-4-2: Calculate MNA matrices, Gm, Qm, F, I, J, I2 by Newton’s iteration methods.
     [PGm, PQm, PF, PI, PJ, PI2] = BSIMCMG('MP',Vdd,Vin,Vout,Vdd);
     [NGm, NQm, NF, NI, NJ, NI2] = BSIMCMG('MN',Vout,Vin,0,0);
-    
+    % chart 3-4-1: update the MNA matrix, G.
     G(1,1) = -PGm(1,1);
     G(2,2) = -PGm(2,2) + -NGm(2,2);
     G(3,3) = -PGm(3,3) + -NGm(1,1);
@@ -49,13 +50,16 @@ function X = INV_DC(Vin, Vdd, error, alpha)
     %rhs = ([-PF(1) + -PF(4); -PF(2) + -NF(2); -PF(3) + -NF(1); 0; 0] + W + -[ NI(1) + NI(4); PI(2) + NI(2); 0; 0; 0] );
     rhs = ([-PF(1) + -PF(4); -PF(2) + -NF(2); -PF(3) + -NF(1); 0; 0] + W + -[ NI(1) + NI(4); PI(2) + NI(2); 0; 0; 0] );
     f = G*[Vdd; Vin; Vout; PI(3) + NI(1); PI(2) + NI(2) ] - rhs;
+    % chart 3-5: ||Xk+1 - Xk|| < error?
     if abs(f(3)) < error
       loop
       break
     end
     %dVout = -1000000*(f(3))/G(3,3);
     %order = floor(log(abs(f(3)))./log(10));
+    % chart 3-6: update the damped Newton’s method to solve FX = GX + W.
     dVout = -alpha*100*f(3)/G(3,3);
+    % chart 3-7: Xn+1(t)=Xn(t). 
     Vout = Vout + dVout;
     
     if mod(loop,500) == 0
