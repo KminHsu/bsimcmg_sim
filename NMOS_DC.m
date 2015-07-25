a5cf19c516529f75919f4d9bbec4309e1a2060ee
JimmYang <stu3181@hotmail.com>
2015-07-25 16:10:24 +0800

    Update NMOS_DC.m

--- NMOS_DC.m
+++ NMOS_DC.m
@@ -32,6 +32,6 @@ function X = MOS_DC(Vd,Vg,Vs,Vb)
     G(row,1:4) = -NGm(row,1:4);
   end
   r = NI + NF - NGm*[Vd;Vg;Vs;Vb];
-  % chart 1-4: solve GX=W
+  % chart 1-4: solve GX=W.
   X = G\ ([-NF(1); -NF(2); -NF(3); -NF(4); 0; 0; 0 ; 0] + W + [r(1);r(2);r(3);r(4);0;0;0;0]);
 end
