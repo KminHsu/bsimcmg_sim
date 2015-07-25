bfca1fdb0e386239e3a3d634c22a07536f4facf3
JimmYang <stu3181@hotmail.com>
2015-07-25 16:18:26 +0800

    Update PMOS_TRAN.m

--- PMOS_TRAN.m
+++ PMOS_TRAN.m
@@ -1,8 +1,9 @@
 % pMOS
 function [Y, time] = PMOS_TRAN(Vdd) 
+  % chart 2-2: initialize the BSIMCMG, and read MOS model card.
   Initialize();
   CreateInst('MP', 'pmos1', 'L=3e-8 TFIN=1.5e-8 NFIN=10.0 NRS=1.0 NRD=1.0');
-  
+  % chart 2-3: initialize matrices and variables.
   % circuit MNA matrix
   G = [ 0  0  0  0  1  0  0  0; 
         0  0  0  0  0  1  0  0;
@@ -37,18 +38,21 @@ function [Y, time] = PMOS_TRAN(Vdd)
   
   G(1:4,1:4) = -Gm(1:4,1:4);
   C(1:4,1:4) = -Qm(1:4,1:4);
-
+  
   r = I + F - Gm*[Vdd;-vin(time(1));0;Vdd];
+  % chart 2-4: using the Backward Euler method to solve the X0 (t = 0, DC analysis).
+  % solve GX0=W.
   X0 = G\ ([-F(1); -F(2); -F(3); -F(4); 0; 0; 0 ; 0] + W + [r(1);r(2);r(3);r(4);0;0;0;0]);
   Xp = X0;
   fn = -[F(1);F(2);F(3);F(4);0;0;0;0] + [r(1);r(2);r(3);r(4);0;0;0;0];
   jp = -1/h*[J(1);J(2);J(3);J(4);0;0;0;0];
   
   Y(1:8,1) = Xp(1:8);
-  
+   % chart 2-5: increasing t.
   for i = 2:length(time)
+    % chart 2-6-2: calculate MNA matrices, Gm, Qm, F, I, J, I2 by Newton’s iteration methods.
     [Gm, Qm, F, I, J, I2] = BSIMCMG('MP',Vdd, -vin(time(i)),0,Vdd);
-
+    % chart 2-6-1: update the MNA matrices, G, C, and W.
     G(1:4,1:4) = -Gm(1:4,1:4);
     C(1:4,1:4) = -Qm(1:4,1:4);
 
@@ -56,14 +60,14 @@ function [Y, time] = PMOS_TRAN(Vdd)
     r = I + F - Gm*[Vdd;-vin(time(i));0;Vdd];
     fn = -[F(1);F(2);F(3);F(4);0;0;0;0] + [r(1);r(2);r(3);r(4);0;0;0;0];
     jn = -1/h*[J(1);J(2);J(3);J(4);0;0;0;0];
-
+    % chart 2-7: prepare W.
     W = [0; 0; 0; 0; Vdd; -vin(time(i)); 0; Vdd] + fn + jn - jp;
     
-    % solve (G+1/h*C)Xn=1/h*C*Xp+W
+    % chart 2-8: solve (G+1/h*C)Xn=1/h*C*Xp+W.
     Xn = M\(1/h*C*Xp+W);
   
     Y(1:8,i) = Xn(1:8);
-    
+    % chart 2-9: Xn(t+1) = Xn(t).
     jp = jn;
     Xp = Xn;
   end
